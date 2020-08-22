fd1568433a7724822b9b3e8f0f38eb0c82bab630
Alex Bobyarchick <anbobyarchick@gmail.com>
2020-08-21 21:04:22 -0400

    fixed homepage image fixed default image

--- src/Containers/ImageImporter/ImageImporter.js
+++ src/Containers/ImageImporter/ImageImporter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import defaultDisplay from '../../assets/website-designs/watermarkSignature.png';
-import { goodNaturePromotional  as promotionalImage} from '../../assets/website-designs/website-designs-index/website-designs-index';
+import promotionalImage from '../../assets/website-designs/GN_Promotional_Logo.png';
 import digitalIndex from '../../assets/artwork/digitalWork/digitalIndex';
 import illustrationsArray from '../../assets/artwork/illustrations/illustrationsIndex';
 import sketchbookIndex from '../../assets/artwork/SketchbookOne/sketchbookIndex';
@@ -43,8 +43,8 @@ const imageImporter = (props) => {
                     />
             ));
         case 'promotional':
-            galleryPaths = promotionalImage;
-                return(galleryPaths);
+            
+                return(<img src={promotionalImage}  alt='default shrug' />);
         default:
             return(
                 <img src={defaultDisplay}  alt='default shrug' />
